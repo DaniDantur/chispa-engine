abe70a55ea38e0bc17e6f1a5b52932d5f2c589ef
Sebastian <seba.alvarez16@gmail.com>
2016-01-05 19:38:20 -0300

    Primeros resultados para mover el robot

--- chispa/ordenar-por-colores.py
+++ chispa/ordenar-por-colores.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
+import time
 import chispaengine
 
 chispa = chispaengine.prender()
+robot = chispa.placas.Duinobot()
 
-
+robot.prender()
+robot.prender_motores()
+time.sleep(1)
+robot.apagar_motores()
 
 
 #robot = chispa.placas.duinobot
