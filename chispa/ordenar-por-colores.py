188cbe0188d64af45987b9163fd8628515d6f4bc
Sebastian <seba.alvarez16@gmail.com>
2016-01-13 12:47:02 -0300

    Cambios en los colores, se agrega ejemplo de ordenar por color con serovs

--- chispa/ordenar-por-colores.py
+++ chispa/ordenar-por-colores.py
@@ -6,13 +6,50 @@ import chispaengine
 chispa = chispaengine.prender()
 
 robot = chispa.placas.Duinobot()
-robot.prender()
-#robot.prender_motores()
-#time.sleep(0.025)
-#robot.apagar_motores()
+camara = chispa.camaras.Netbook()
 
-robot.prender_motor(motor=0, velocidad=-75)
-time.sleep(1)
-robot.apagar_motores()
+robot.prender()
+camara.prender()
+
+colores_a_buscar = ["verde", "rojo"]
+hay_objeto_de_color = False
+
+
+while(True):
+    imagen = camara.traer_imagen()
+    imagen.mostrar("Camara")
+
+    zona_elegir_color = imagen.recortar([80, 200], [300, 450])
+    zona_elegir_color.mostrar("Zona para elegir color")
+
+
+    for color in colores_a_buscar:
+        if(hay_objeto_de_color):
+            color = hay_objeto_de_color
+
+        #print "Busco tapitas de color " + color
+
+        m, objetos = zona_elegir_color.buscar_objetos_por_color(color)
+        m.mostrar("M")
+
+        if(objetos is not None):
+            print "Hay tapita de color " + color
+
+            hay_objeto_de_color = color
+
+            if(hay_objeto_de_color == "verde"):
+                angulo = 90
+            elif(hay_objeto_de_color == "rojo"):
+                angulo = 180
+
+            robot.prender_servo(servo=0, angulo=angulo)
+
+        else:
+            hay_objeto_de_color = None
+            robot.prender_servo(servo=0, angulo=0)
+
+
+    chispa.esperar(0.025)
+
 
 
